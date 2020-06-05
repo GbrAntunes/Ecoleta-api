098f6e0542fa44272df7e48155ddcdb911853bf1
Gabriel antunes <gbr.antunes@gmail.com>
2020-06-05 09:07:02 -0400

    Rota pronta para receber o envio de images e retornando a url para acesso da imagem enviada

--- src/routes.ts
+++ src/routes.ts
@@ -1,4 +1,6 @@
 import express from 'express'
+import multer from 'multer'
+import multerConfig from './config/multer'
 
 import PointsController from './controllers/PointsController'
 import ItemsController from './controllers/ItemsController'
@@ -6,10 +8,15 @@ import ItemsController from './controllers/ItemsController'
 const pointsController = new PointsController()
 const itemsController = new ItemsController()
 const routes = express.Router()
+const upload = multer(multerConfig)
 
 routes.get('/items', itemsController.index)
 
-routes.post('/points', pointsController.create)
+/**
+ * Na rota que vamos receber a imagem, podemos passar antes do método do controller,
+ * um novo parâmetro com as configurações do multer e o nome do campo no formulário
+ *  */ 
+routes.post('/points', upload.single('image'), pointsController.create)
 routes.get('/points/', pointsController.index)
 routes.get('/points/:id', pointsController.show)
 
